12e997c87b6ca6c9cc088215fbde7627c7b9a2e7
Alex2910 <alexantonioangel2910@gmail.com>
2023-08-06 23:43:12 -0600

    Practica 5 Finalizado v2.0

--- practica5.js
+++ practica5.js
@@ -1,10 +1,22 @@
-function validacion(arreglo, numero){
+
+/*Diseñar  un  programa  en  donde  se  capture  un  arreglo (o  lista)  de  5  elementos  y  2  números  enteros,  
+el  programa tendrá que indicar alguno de los siguientes casos: 
+•Los dos números enteros capturados se encuentran en el arreglo 
+•Solo el primer número capturado se encuentra en el arreglo 
+•Solo el segundo numero capturado se encuentra en el arreglo
+•Ninguno de los dos números se encuentra en el arreglo
+
+*/
+function validacion(arreg, numero){
 let val=0
-for(const i of arreglo){//esta opcion dentro de for es solamente para arreglos en una variable cualquiera seria for (let i=0,i..5 )
-    if(arreglo[i]==numero) val++
+
+for(i=0; i<arreg.length ; i++){//esta opcion dentro de for es solamente para arreglos en una variable cualquiera seria for (let i=0,i..5 )
+    if(arreg[i]==numero) val++
+  
 }
 return val
 }
+
 function into_val(){
 let valor=prompt("introduce el valor para verificar","")
 
@@ -14,21 +26,44 @@ return valor
 }
 
 function arrays(){
-    let arreglo = [1,2,3,4,5,6]
+    let arreglo = []
+    
+    
+    let valMin= (Math.floor(Math.random()* parseInt(100))+parseInt(1))
+    if (valMin>=90)return arrays()
+    let  valMax=valMin+10
+        
+       
+
+        alert("se generara una lista del "+ valMin+" al "+valMax)
+    let almacen=" "
+    let valRang= prompt("Agrega el valor del rango","")
+        
+    for(i=0; i<valRang; i++)   {
+        arreglo[i]=(Math.floor(Math.random()* (parseInt(valMax)-parseInt(valMin)+parseInt(1)))+parseInt(valMin))
+     
+        almacen=almacen+" "+arreglo[i]
+    }
+   // alert(arreglo[0])
+    alert(almacen)
     return arreglo
+    
 }
+
+
+
+let array=arrays()
 
 let valor1=into_val()
 let valor2=into_val()
-let array=arrays()
 let val1=validacion(array,valor1)
 let val2=validacion(array,valor2)
 
 if (val1 != 0) alert("El valor "+valor1+" se repite "+val1+" veces");
-else alert("El valor "+ valor1 +" no se aparece en la lista")
+else alert("El valor "+ valor1 +" no aparece en la lista")
 
 if (val2 != 0) alert("El valor "+valor2+" se repite "+val2+" veces");
-else alert("El valor "+ valor2 +" no se aparece en la lista")
+else alert("El valor "+ valor2 +" no aparece en la lista")
 
 
 
