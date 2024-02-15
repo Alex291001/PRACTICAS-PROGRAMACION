e009df19b433f872ac7964dc1e3b2b3864baa563
Alex2910 <alexantonioangel2910@gmail.com>
2024-02-14 18:45:33 -0600

    Update Biblioteca java

--- java/biblioteca/src/Autor.java
+++ java/biblioteca/src/Autor.java
@@ -21,4 +21,7 @@ public class Autor {
     public Autor() {
 
     }
+
+
+
 }
--- java/biblioteca/src/TestBiblioteca.java
+++ java/biblioteca/src/TestBiblioteca.java
@@ -1,15 +1,20 @@
-import javax.lang.model.type.NullType;
+import java.util.ArrayList;
 import java.util.Scanner;//importar entrada de datos
 
 public class TestBiblioteca {
     public static Autor[] autores = new Autor[10];
+    //El ArrayList se utiliza para que nosotros podamos tener mejor interaccion con nuestro lista
+    public static ArrayList<Autor> lista = new ArrayList<Autor>();
+
+    public static ArrayList<Integer> lista2 = new ArrayList<>();//para probar como funciona
     public static int pos = 0;
 
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);//Entrada de datos desde el teclado
-        // Autor autor1 = new Autor();
-        //  Autor autor2 = new Autor(1, "Isaac", "Perez", "Campo", "Oaxaca");
+        //  Autor autor = new Autor(1,"alex","Antonio","Angel","oaxaca");
+        //Autor autor2 = new Autor(2, "Isaac", "Perez", "Campo", "Oaxaca");
+        int[] numero = {1, 2, 3};
         int op, i, id;
         int index;
         boolean condition = true;
@@ -37,23 +42,38 @@ public class TestBiblioteca {
 
 
         do {
-            System.out.println("**********BIBLIOTECA*****8");
+
+            // System.out.println(lista2);
+            for (i = 0; i < lista.size(); i++)
+
+                autores[i] = lista.get(i);
+
+            //System.out.println(lista);
+            System.out.println("**********BIBLIOTECA******");
             System.out.println("1.- Agregar");
             System.out.println("2.- Buscar");
             System.out.println("3.- Listar");
             System.out.println("4.- Eliminar");
+            System.out.println("5.- Modificar");
             System.out.println("0.- Salir");
 
             op = lector.nextInt();//agregar texto
 
             switch (op) {
+
                 case 0:
                     System.out.println("Fin del programa...");
                     break;
                 case 1:
                     System.out.println("AGREGAR NUEVO AUTOR:");
                     if (pos < 10 && autores[pos] == null) {
-                        autores[pos] = registrarAutor(pos);
+                        autores[pos] = registrarAutor(pos, true);
+
+
+                        // lista2.add(numero[pos]);
+
+                        //lista.add(autores[pos]);
+
 
                     } else {
                         System.out.println("Ya tienes registrado a un autor");
@@ -68,37 +88,42 @@ public class TestBiblioteca {
                     System.out.println("Agregar id del autor del 0 al 9");
                     id = lector.nextInt();
                     //Se va directamente con el metodo buscar autor y manda 1 0 -1
-                    index = BuscarAutor(id, 1);
+                    index = BuscarAutor(id, 1, false);
                     //Si es -1 entonces significa que no tiene nada registrado en ese autor
                     if (index == -1) {
 
-                        System.out.println("No se encontro el autor solicitado...");
+                        System.out.println("No se encontro el autor solicitado...Se agregaran nuevos dato de casilla vacía");
                         System.out.println("");
                         System.out.println("Inserta nuevos datos");
-                        autores[pos] = registrarAutor(pos);
+                        autores[pos] = registrarAutor(id, false);
 
                     }
                     break;
 
                 case 3:
                     System.out.println("LISTAR AUTORES");
-                    for (i = 0; i < 10; i++) {
-                        if (autores[i] != null) {
-                            System.out.println("id: " + autores[i].id);
-                            System.out.println("Nombre: " + autores[i].nombre + " " + autores[i].aPaterno + " " + autores[i].aMaterno);
-                            condition = false;
-                        }
+                    for (i = 0; i < lista.size(); i++) {
+
+                        System.out.println("id: " + lista.get(i).id);
+                        System.out.println("Nombre: " + lista.get(i).nombre + " " + lista.get(i).aPaterno + " " + lista.get(i).aMaterno);
+
 
                     }
 
-                    if (condition) System.out.println("***LA LISTA ESTA VACÍA*****");
+                    if (lista.isEmpty()) System.out.println("***LA LISTA ESTA VACÍA*****");
                     break;
 
                 case 4:
                     System.out.println("ELIMINAR AUTOR");
                     System.out.println("Agregar id del autor del 0 al 9");
                     id = lector.nextInt();
-                    index = BuscarAutor(id, 2);
+                    index = EliminarAutor(id, 2, false);
+                    break;
+                case 5:
+                    System.out.println("MODIFICAR AUTOR");
+                    System.out.println("Agregar id del autor del 0 al 9");
+                    id = lector.nextInt();
+                    index = ModificarAutor(id, 2, true);
                     break;
                 default:
                     System.out.println("VALORES INCORRECTOS.. INTENTA NUEVAMENTE..");
@@ -110,10 +135,11 @@ public class TestBiblioteca {
 
     }
 
-    public static Autor registrarAutor(int n) {
+    public static Autor registrarAutor(int n, boolean opc) {
         Scanner lector = new Scanner(System.in);//Entrada de datos desde el teclado
         int id;
-        pos++;
+
+
         String nombre;
         String aPaterno;
         String aMaterno;
@@ -132,28 +158,43 @@ public class TestBiblioteca {
         ciudad = lector.next();//.Entrada de..
 
         Autor autor = new Autor(id, nombre, aPaterno, aMaterno, ciudad);
+        // lista.add(autor);
 
         System.out.println("AUTOR REGISTRADO..");
-
+        if (opc) pos++;
+        lista.add(autor);
         return autor;
 
     }
 
     //Este metodo sirve para que pueda hacer la busqueda del id, se utilizara demasiado.
-    public static int OperacionBusqueda(int id,int opc){
-
+    public static int OperacionBusqueda(int id, int opc, boolean n) {
+        Scanner lector = new Scanner(System.in);
         int i;
         for (i = 0; i < 9; i++) {
             //comparamos si autores es nulo si es asi no lee el array y no genera ERROR
             if (autores[i] != null && autores[i].id == id) {
                 //
+                //System.out.println(lista2.contains(i));
+                System.out.println("autores= id: " + autores[i].id);
+                System.out.println("lista =id: " + lista.get(i).id);
 
-                System.out.println("id: " + autores[i].id);
-
-                System.out.println("Nombre: " + autores[i].nombre + " " + autores[i].aPaterno + " " + autores[i].aMaterno);
+                System.out.println("autores= Nombre: " + autores[i].nombre + " " + autores[i].aPaterno + " " + autores[i].aMaterno);
+                System.out.println("listaArray= Nombre: " + lista.get(i).nombre + " " + lista.get(i).aPaterno + " " + lista.get(i).aMaterno);
                 //regresa 1 para dar a entender que se ejecuto y no necesita nada mas
                 if (opc == 2) {
-                    System.out.println("¿Deseas eliminar al autor? y/n");
+                    System.out.println("¿Deseas eliminar al autor seleccionado? y=1/n=0");
+                    int desicion = lector.nextInt();
+                    if (desicion == 1) {
+                        lista.remove(i);
+                        autores[i] = lista.get(i);
+
+                    }
+
+                    if (n) {
+                        System.out.println("MODIFICACION DE AUTOR");
+                        autores[i] = registrarAutor(i, false);
+                    }// else pos--;
 
                 }
                 return 1;
@@ -167,9 +208,21 @@ public class TestBiblioteca {
 
     }
 
-    public static int BuscarAutor(int id, int opc) {
+    public static int BuscarAutor(int id, int opc, boolean n) {
 //Se manda al metodo OperacionBusqueda que es el cual realiza todo el movimiento y es sumamente reutilizable
-       return OperacionBusqueda(id,opc);
+        return OperacionBusqueda(id, opc, n);
+
+    }
+
+    public static int EliminarAutor(int id, int opc, boolean n) {
+//Se manda al metodo OperacionBusqueda que es el cual realiza todo el movimiento y es sumamente reutilizable
+        return OperacionBusqueda(id, opc, n);
+
+    }
+
+    public static int ModificarAutor(int id, int opc, boolean n) {
+//Se manda al metodo OperacionBusqueda que es el cual realiza todo el movimiento y es sumamente reutilizable
+        return OperacionBusqueda(id, opc, n);
 
     }
 
